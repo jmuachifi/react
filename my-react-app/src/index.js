ddb6afadfb8bfbb5b31c7df53bfeea283327aeaf
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-07-27 23:51:30 +0100

    ES6 with react JS

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,14 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myFirstElement =  <h1>Hello React!</h1>
+const myFirstElement =  <h1 className='text text-center'>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( myFirstElement );
 
+//h1>Array Methods</h1>
+const myArray = ['apple', 'banana', 'orange'];
+const myList = myArray.map(item => <p>{item}</p>);
+ReactDOM.render(myList, document.getElementById('root'));
 
 
  /*root.render( <React.StrictMode>
